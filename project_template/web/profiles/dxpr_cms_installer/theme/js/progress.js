b9f2c4927f2c498351aded9db8b17b61216769ae
Jurriaan Roelofs <jur@dxpr.com>
2025-06-30 16:36:47 +0200

    Dxpr > DXPR

--- project_template/web/profiles/dxpr_cms_installer/theme/js/progress.js
+++ project_template/web/profiles/dxpr_cms_installer/theme/js/progress.js
@@ -3,7 +3,7 @@
   /**
    * Renders a client-side progress bar.
    *
-   * This is for the Dxpr CMS installer and is not meant to be reused.
+   * This is for the DXPR CMS installer and is not meant to be reused.
    */
   Drupal.theme.progressBar = function (id) {
     const escapedId = Drupal.checkPlain(id);
