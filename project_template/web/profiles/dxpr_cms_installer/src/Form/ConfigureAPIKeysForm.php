e640079f2198cc84d2b1d9c563c9a8ed26295041
Vitalii Nechai <nechai.dev@gmail.com>
2025-08-05 21:20:32 +0300

    #41: Replace "tmgmt_google" module with "ai_tmgmt"

--- project_template/web/profiles/dxpr_cms_installer/src/Form/ConfigureAPIKeysForm.php
+++ project_template/web/profiles/dxpr_cms_installer/src/Form/ConfigureAPIKeysForm.php
@@ -120,18 +120,6 @@ class ConfigureAPIKeysForm extends FormBase implements ContainerInjectionInterfa
       '#required' => TRUE,
     ];
 
-
-
-    // if (isset($install_state['dxpr_cms_installer']['enable_multilingual']) &&
-    // $install_state['dxpr_cms_installer']['enable_multilingual']) {
-    $form['google_translation_key'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Google Cloud Translation API key (optional)'),
-      '#description' => $this->t('Get a key from <a href="https://console.cloud.google.com/marketplace/product/google/translate.googleapis.com" target="_blank">cloud.google.com</a>.'),
-    ];
-    // }
-
-
     $form['actions'] = [
       'continue' => [
         '#type' => 'submit',
@@ -179,11 +167,6 @@ class ConfigureAPIKeysForm extends FormBase implements ContainerInjectionInterfa
       catch (\Exception $e) {
         $this->messenger()->addError($this->t('An error occurred while saving the DXPR Builder key: @error', ['@error' => $e->getMessage()]));
       }
-    }
-
-    $google_translation_key = $form_state->getValue('google_translation_key');
-    if (!empty($google_translation_key)) {
-      $this->configFactory->getEditable('tmgmt.translator.google')->set('settings.api_key', $google_translation_key)->save();
     }
 
     // Configure DXPR AI provider using the DXPR Builder key
